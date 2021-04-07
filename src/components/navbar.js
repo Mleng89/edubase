5e93226f225021eb3368e45caaecb5e5692e4629
Matthew Leng <mleng89@gmail.com>
2021-04-07 00:05:36 -0400

    Edit: Changed burger from anchor to button

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,20 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './navbar.css';
 export default function Navbar() {
 	const navbarLinks = document.getElementsByClassName('navbar-links');
-	console.log('navbarLinks', navbarLinks[0].classList.toggle);
+	console.log('what is navbarLinks before effect', navbarLinks);
+	/** This is required as navbar-links is not loaded initially with the page
+	 *  Once navbarLinks updates, useEffect will rerender it allowing the hamburger to work when
+	 *  the browser past 750px
+	 * */
+	useEffect(() => {}, [navbarLinks]);
+
 	return (
 		<div className='navbar'>
 			<div className='brand-title'> EduBase </div>
-			<a
-				href='/#'
+			<button
 				className='toggle-button'
 				onClick={() => navbarLinks[0].classList.toggle('active')}
 			>
 				<span className='bar'></span>
 				<span className='bar'></span>
 				<span className='bar'></span>
-			</a>
+			</button>
 			<div className='navbar-links'>
 				<ul>
 					<li>
