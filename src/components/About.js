5ab41c24bafb91b406a6743ec5632e656b84287f
Matthew Leng <mleng89@gmail.com>
2021-04-22 20:51:10 -0400

    Added: About page -- footer

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import './About.css';
+import firebaselogo from '../Images/firebase.svg';
+import reactlogo from '../Images/react.svg';
 export default function About() {
 	return (
 		<div>
 			<header>About EduBase (Name in progress)</header>
-			<footer>This project is built on: </footer>
+			<body className='about-body'>
+				<main className='about-content'></main>
+				<footer>
+					<p>This project is built on:</p>
+					<img src={firebaselogo} alt='Firebase' />
+					<img src={reactlogo} alt='React' />
+				</footer>
+			</body>
 		</div>
 	);
 }
