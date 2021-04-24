77eeebe40d846f8074e1788705af8f45ee298d78
Matthew Leng <mleng89@gmail.com>
2021-04-23 20:11:32 -0400

    Added: Firebase auth

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+var firebaseConfig = {
+	apiKey: 'AIzaSyDLRmuZ3D08Bs2xJT9BqWxY3RVi7uQmrrI',
+	authDomain: 'edu-develop-b5763.firebaseapp.com',
+	projectId: 'edu-develop-b5763',
+	storageBucket: 'edu-develop-b5763.appspot.com',
+	messagingSenderId: '773301275257',
+	appId: '1:773301275257:web:258133a289acb3585862a8',
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
