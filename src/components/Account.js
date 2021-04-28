fd241e762bdc1a696e7765fc46583d3a2b219486
Matthew Leng <mleng89@gmail.com>
2021-04-28 13:22:52 -0400

    WIP: Persistent Google OAuth via firebase

--- src/components/Account.js
+++ src/components/Account.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import './Account.css';
-import { firebaseConfig } from '../firebase';
-import { BrowserRouter, NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+import app from '../Db/firebase';
+
 export default function Account() {
 	const [state, setState] = useState({
 		username: '',
@@ -13,16 +14,53 @@ export default function Account() {
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
-		console.log('Submit clicked');
+	};
+
+	const googleSubmit = (e) => {
+		console.log('clicked');
+		e.preventDefault();
+		const provider = new app.firebase_.auth.GoogleAuthProvider();
+		console.log('what is provider', provider);
+		/***
+		 * Firebase auth
+		 * For
+		 * Google
+		 */
+		app.firebase_
+			.auth()
+			.signInWithPopup(provider)
+			.then((result) => {
+				let credential = result.credential;
+				let token = credential.accessToken;
+				let user = result.user;
+				console.log('what is result', result);
+				console.log('what is credential', credential);
+				console.log('what is token?', token);
+				console.log('what is user?', user);
+			})
+			.catch((err) => {
+				// const errCode = err.code;
+				// const errMsg = err.message;
+				// const email = err.email;
+				// const credential = err.credential;
+				console.log('Error:', err);
+			});
+		console.log('what is currentUser', app.firebase_.auth().currentUser);
 	};
 	const handleChange = (e) => {
 		setState({ ...state, [e.target.id]: e.target.value });
 	};
+	const userLoggedin = app.firebase_.auth().currentUser;
 	console.log('what is state?', state);
 	return (
 		<div className='account-container'>
 			<div className='account-signin'>
 				<h2>Account Login</h2>
+				{/* CHECKING IF THERE IS A USER */}
+				{userLoggedin
+					? `Hello ${app.firebase_.auth().currentUser.displayName}`
+					: null}
+
 				<form className='sign-in' onSubmit={handleSubmit}>
 					<label>
 						<input
@@ -57,6 +95,9 @@ export default function Account() {
 							Sign up
 						</NavLink>
 					</div>
+				</form>
+				<form onSubmit={() => googleSubmit}>
+					<button onClick={googleSubmit}> Log in with Google</button>
 				</form>
 			</div>
 		</div>
