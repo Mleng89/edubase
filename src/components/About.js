97bdaba178466d43d19963bdc3cbccdf05ec09ea
Matthew Leng <mleng89@gmail.com>
2021-04-28 13:23:07 -0400

    Testing: Persistence in other pages

--- src/components/About.js
+++ src/components/About.js
@@ -1,19 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './About.css';
 import firebaselogo from '../Images/firebase.svg';
 import reactlogo from '../Images/react.svg';
+import app from '../Db/firebase';
 export default function About() {
+	// console.log('what is in my app', app);
+
+	const userLoggedin = app.firebase_.auth().currentUser.displayName
+		? app.firebase_.auth().currentUser.displayName
+		: '';
+
 	return (
 		<div>
 			<header>About EduBase (Name in progress)</header>
-			<body className='about-body'>
+			{userLoggedin
+				? `Hello ${app.firebase_.auth().currentUser.displayName}`
+				: ''}
+			<div className='about-body'>
 				<main className='about-content'></main>
+
 				<footer>
 					<p>This project is built on:</p>
 					<img src={firebaselogo} alt='Firebase' />
 					<img src={reactlogo} alt='React' />
 				</footer>
-			</body>
+			</div>
 		</div>
 	);
 }
