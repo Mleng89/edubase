913ba3815cb49a17cc34866cb446eca7b7051703
Matthew Leng <mleng89@gmail.com>
2021-05-05 14:17:19 -0400

    Added: Added a homepage component

---
+++ src/components/Homepage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Homepage.css';
+
+export default function Homepage() {
+	return (
+		<div className='homepage-container'>
+			<h1>Welcome to EduBase(Name in progress!)</h1>
+		</div>
+	);
+}
