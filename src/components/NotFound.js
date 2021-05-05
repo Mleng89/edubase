050e23564bf432ebbb55b54a77e8d8683da3f0d7
Matthew Leng <mleng89@gmail.com>
2021-05-05 17:09:24 -0400

    Added: Image to Not Found paeg

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './NotFound.css';
+import notfoundimg from '../Images/notfoundimg.svg';
 
 export default function NotFound() {
 	return (
 		<div className='notfound-container'>
 			<h2>Error!</h2>
+			<img src={notfoundimg} alt='Not Found' />
 			<h3>The page you are looking for is not here!</h3>
 			<h3>Please click on the top left to return to the home page.</h3>
 		</div>
