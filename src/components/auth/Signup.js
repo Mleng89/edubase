9a2587075f29085e4226d2cf49485c274fb19ea0
Matthew Leng <mleng89@gmail.com>
2021-05-05 15:06:52 -0400

    Added: Users can now sign up with email and password

--- src/components/auth/Signup.js
+++ src/components/auth/Signup.js
@@ -5,67 +5,88 @@ import app from '../../Db/firebase';
 import { useAuth } from '../../Db/AuthContext';
 
 export default function Login() {
-	const [state, setState] = useState({
-		email: '',
-		password: '',
-		confirmpassword: '',
-		//ERROR HANDLING
-		// errorName: false,
-		// errorPassword: false,
-	});
+	// const [state, setState] = useState({
+	// 	email: '',
+	// 	password: '',
+	// 	confirmpassword: '',
+	// 	//ERROR HANDLING
+	// 	// errorName: false,
+	// 	// errorPassword: false,
+	// });
+	const emailRef = useRef();
+	const passwordRef = useRef();
+	const passwordConfirmRef = useRef();
+	const { signup } = useAuth();
+	const [error, setError] = useState('');
+	const [loading, setLoading] = useState(false);
+	const history = useHistory();
 
-	const handleSubmit = (e) => {
+	async function handleSubmit(e) {
 		e.preventDefault();
+		if (passwordRef.current.value !== passwordConfirmRef.current.value) {
+			return setError('Passwords do not match');
+		}
+		if (passwordRef.current.value.length < 6) {
+			return setError('Please enter a password longer than 6 characters');
+		}
+		try {
+			setError('');
+			setLoading(true);
+			await signup(emailRef.current.value, passwordRef.current.value);
+			history.push('/dashboard');
+		} catch {
+			setError('Failed to create an account');
+		}
+		setLoading(false);
+		// app.firebase_
+		// 	.auth()
+		// 	.createUserWithEmailAndPassword(state.email, state.password)
+		// 	.then((userCredential) => {
+		// 		let user = userCredential.user;
+		// 	})
+		// 	.catch((err) => {
+		// 		console.log('Error:', err.code, err.message);
+		// 	});
+	}
 
-		app.firebase_
-			.auth()
-			.createUserWithEmailAndPassword(state.email, state.password)
-			.then((userCredential) => {
-				let user = userCredential.user;
-			})
-			.catch((err) => {
-				console.log('Error:', err.code, err.message);
-			});
-	};
-
-	const handleChange = (e) => {
-		setState({ ...state, [e.target.id]: e.target.value });
-	};
-
+	// const handleChange = (e) => {
+	// 	setState({ ...state, [e.target.id]: e.target.value });
+	// };
 	return (
 		<div className='Login-container'>
 			<div className='Login-signin'>
 				<h2>Sign up</h2>
-
+				{/* IF THERE IS AN ERROR, DISPLAY IT */}
+				{error && `${error}`}
 				<form className='sign-in' onSubmit={handleSubmit}>
 					<label>
 						<input
 							type='email'
 							id='email'
-							value={state.email}
+							// value={state.email}
 							placeholder='E-mail'
-							onChange={handleChange}
-							// ref={emailRef}
+							// onChange={handleChange}
+							ref={emailRef}
 						/>
 					</label>
 					<label>
 						<input
 							type='password'
 							id='password'
-							value={state.password}
+							// value={state.password}
 							placeholder='Password'
-							onChange={handleChange}
-							// ref={passRef}
+							// onChange={handleChange}
+							ref={passwordRef}
 						/>
 					</label>
 					<label>
 						<input
 							type='password'
 							id='confirmpassword'
-							value={state.confirmpassword}
+							// value={state.confirmpassword}
 							placeholder='Confirm password'
-							onChange={handleChange}
-							// ref={passConfirmRef}
+							// onChange={handleChange}
+							ref={passwordConfirmRef}
 						/>
 					</label>
 
