a385dc2579addc26f4455edf0d14ca5bfb29efde
Matthew Leng <60058291+Mleng89@users.noreply.github.com>
2022-09-22 17:09:53 -0400

    FE: Upload File page and route created

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -7,6 +7,15 @@ export default function Dashboard() {
 	const [error, setError] = useState('');
 	const { currentUser, logout } = useAuth();
 	const history = useHistory();
+	async function handleClick() {
+		setError('');
+
+		try {
+			history.push('/files');
+		} catch {
+			setError('Failed to find path.');
+		}
+	}
 	async function handleLogout() {
 		setError('');
 
@@ -14,13 +23,20 @@ export default function Dashboard() {
 			await logout();
 			history.push('/');
 		} catch {
-			setError('Failed to log out');
+			setError('Failed to log out.');
 		}
 	}
+
 	return (
 		<div className='dashboard-container'>
-			<h1> Welcome {currentUser.email}! </h1>
-			<button onClick={handleLogout}>Log out</button>
+			<>
+				<h1> Welcome {currentUser.email}! </h1>
+				<div className='file-upload-container'>
+					<button onClick={handleClick}>File uploads</button>
+				</div>
+			</>
+
+			{/* <button onClick={handleLogout}>Log out</button> */}
 		</div>
 	);
 }
